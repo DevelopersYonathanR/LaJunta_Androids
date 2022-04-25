b6389a27ef8516048762ea4c964d4a16cc106ca7
YonathanRios2022 <yonathanalejandrorios@gmail.com>
2022-04-25 17:30:00 -0400

    commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+
+<body id="home">
+    <header class="header">
+        <div class="container">
+            <div class="titulo">LOGO</div>
+            <nav class="menu">
+                <ul>
+                    <li>
+                        <a href="#">Servicios</a>
+                    </li>
+                    <li>
+                        <a href="#">Mi Cuenta</a>
+                        <ul class="submenu">
+                            <li><a href="login.php">Iniciar sesion</a></li>
+                            <li><a href="#">Crear cuenta</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section class="fondo">
+        <div class="subtitulo">
+            Turismo Rural La Junta
+        </div>
+        <div class="subtitulo-desc">
+            Mas de 8 años ofreciendo experiencias
+            <br>
+            de Calidad a nuestros Clientes.
+        </div>
+        <div class="btn-info">
+            <a href="#info">Informacion</a>
+        </div>
+    </section>
+</body>
+
+<section id="info">
+    <div class="info-complete">
+        <p>
+            Con ubicación en La regio de los ríos, Comuna de Lago Ranco,
+
+            entre sus servicios se encuentra El arriendo de cabañas, Quincho,
+
+            tinajas de relajación, ecoturismo, turismo aventura, hospedaje.
+        </p>
+    </div>
+
+    <div class="btn-volver">
+        <a href="#home">=</a>
+    </div>
+</section>
+
+</html>
---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/styles.css">
+    <title>Document</title>
+</head>
+
+<body id="home">
+   
+
+    <section class="fondo">
+
+    <div class="login-page">
+        <div class="form">
+
+            <h2 class="titulo2"> Iniciar Sesion</h2>
+
+            <form class="login-form" action="index.php">
+                <input type="text" placeholder="Usuario" />
+                <input type="password" placeholder="Contraseña" />
+                <button>Iniciar Sesion</button>
+                <p class="message">No estas registrado? <a href="#">crear cuenta aqui</a></p>
+            </form>
+        </div>
+    </div>
+
+    </section>
+
+    
+
+
+</body>
+
+
+
+</html>
